c8f8043444789d7ccf3b96818b7cf62f3faf9b6a
sankaraitha <sivasankaraitha@gmail.com>
2016-10-04 13:47:21 +0530

    Checked..

--- email.php
+++ email.php
@@ -1,7 +1,22 @@
 <?php
-$headers = 'MIME-Version: 1.0' . "\r\n";
+/*$headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From: [email]sivasankaraitha@gmail.com[/email]' . "\r\n";
+$headers .= 'From: [email]sivasankaraitha@gmail.com[/email]' . "\r\n";*/
 
-mail("sankaraitha@gmail..com", "Test mail", "Test body", $headers);
+/*if(mail("sankaraitha@gmail..com", "Test mail", "Test body", 'sivasankaraitha@gmail.com'))
+echo "sent";
+echo "sent";*/
+
+if(mail(
+     'sivasankaraithamail@gmail.com',
+     'Works!',
+     'An email has been generated from your localhost, congratulations!','sivasankaraitha@gmail.com'))
+{
+  echo "Mail sent Successfully";
+}
+else
+  {
+  echo "Mail does n't sent";
+}
+
 ?>
